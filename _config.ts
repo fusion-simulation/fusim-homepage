a82700e5e9278e6130265f5897ae9606e7993d7e
eziosweet <cn.djx@outlook.com>
2025-12-19 21:00:14 +0800

    251219 界面优化

--- _config.ts
+++ _config.ts
@@ -9,7 +9,8 @@ import icons from "lume/plugins/icons.ts";
 import cssnano from "npm:cssnano@7.1.2"; 
 const site = lume();
 
-site.data("slogan","打造新一代聚变等离子体仿真软件")
+site.data("slogan","打造新一代聚变仿真工业软件")
+site.data("slogan_2","探索聚变仿真软件，为实现终极能源赋能")
 site.data("description","聚核智算（FUSIM）是一家聚变等离子体软件SaaS服务商，致力于为科研机构和企业提供高效、精准的仿真解决方案，推动聚变能源技术的发展。")
 site.data("icp","鄂ICP备2025165543号-1")
 site.data("email","admin@fusim.cn")
